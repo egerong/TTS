aa3e2fbde13325efd394a6b022860b0193deb3be
egerong <egert@isiro.ee>
2023-04-12 13:23:19 +0000

    Add koneveeb formater

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -601,3 +601,22 @@ def kss(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
             text = cols[2]  # cols[1] => 6월, cols[2] => 유월
             items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
     return items
+
+def koneveeb(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
+    """Normalizes the metadata for edited Estonian speech datasets from
+    koneveeb.ee to TTS format"""
+    items = []
+    for corpus_dir in os.listdir(root_path):
+        speaker_name = corpus_dir.split("_")[0]
+        txt_file = os.path.join(root_path, corpus_dir, meta_file)
+        if not os.path.exists(txt_file):
+            continue
+        with open(txt_file, "r", encoding="utf-8") as ttf:
+            for line in ttf:
+                cols = line.split("|")
+                if not cols[0].endswith(".wav"):
+                    cols[0] += ".wav"
+                wav_file = os.path.join(root_path, corpus_dir, "wavs", cols[0])
+                text = cols[1]
+                items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
+    return items
